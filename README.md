2ce3e6285e9749ddb9c069a62f4659341b8ed94e
Svxy <aidan61605@gmail.com>
2023-12-16 19:35:00 -0500

    purge

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align=center>Official personal portfolio.</h1>
+
+<br>
+
+<p align=center>View here:</p>
+
+<p align=center><b>https://tnyavnto.com</b></p>
+
+<br>
+<hr>
+
+<h4 align=center>Make your one!</h4>
+
+<p align=center>Note <a href='/LICENSE'>GPL 3-0 License</a></p>
+
+<hr>
+
+<h3 align=center>How to download and modify:</h3>
+
+<br>
+
+> - First download <a href='https://git-scm.com/downloads'>git</a> and <a href='https://code.visualstudio.com/'>vscode</a> (if not already installed)
+
+> - Once VSCode is launched in a new folder use this shortcut; <kbd>CTRL</kbd>+<kbd>SHIFT</kbd>+<kbd>`</kbd>
+
+- Then type this command:
+
+```shell
+git clone https://github.com/Svxy/svxy.github.io
+```
+
+- After in File Explorer, open the folder called "svxy.github.io", <kbd>CTRL</kbd>+<kbd>A</kbd> then drag them to the folder you just made.
+- Delete the now empty folder and your done.
+
+<br>
+
+<h3 align=center>Happy editing :)</h3>
---
+++ docs/js/script.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 5000,
+    pagination: false,
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Mainer📍", "Frontend Web Developer ✔", "Web Game Developer 🎮", "Discord Bot Developer 🤖", "Apprentice Auto & Diesel Tech", "Cannabis Enthusiast 🌳", "Developing Simple Sites For Free ❤", "Contact me today 📩"],
+    typeSpeed: 20,
+    startDelay: 1420,
+    showCursor: false,
+    loop: true,
+    backDelay: 1420,
+    backSpeed: 20,
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
+  });
+
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
+
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#ff0000",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $(".counter").each(function () {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+        element.countup(endVal);
+      });
+      countUpFinished = true;
+    }
+  });
+
+  $("[data-fancybox]").fancybox();
+  $("#filters a").click(function () {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false,
+      },
+    });
+    return false;
+  });
+
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $("body");
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
+});
+
+const countupEl = document.querySelector('.countup');
+const username = 'Svxy';
+
+fetch(`https://api.github.com/users/${username}`)
+  .then(response => response.json())
+  .then(data => {
+    const repoCount = data.public_repos;
+    countupEl.dataset.count = repoCount;
+    countupEl.textContent = repoCount;
+  })
+  .catch(error => console.error(error));
